d1017f9496db949ef1faa20d7fe04366be907af8
Jonas Trittler <mail@jonas-trittler.de>
2020-10-29 14:04:33 +0100

    Changed tests

--- tests/Example.test.ts
+++ tests/Example.test.ts
@@ -1,13 +1,4 @@
 
-/*
-test('TestClassTest', done => {
-    const test = new ECApp(5);
-    expect(test.theNumber).toBe(5);
-    done();
-});
-*/
-
-
 test('Pass', async done =>{
     console.log('Feel free to add tests here');
     done();
